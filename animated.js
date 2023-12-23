b26195b4fd3e7da1faf8b032758827e08007b6fa
HuzaifaInshal <huzaifainshal@gmail.com>
2023-12-24 00:00:03 +0500

    added the Out functionality

--- animated.js
+++ animated.js
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", function() {
         const windowHeight = window.innerHeight || document.documentElement.clientHeight;
         return rect.bottom <= windowHeight * 0.8;
       }
+        function isScrolledOutView(el) {
+        const rect = el.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        return rect.bottom <= windowHeight * 0.2;
+      }
 
     elements.forEach((element)=>{
         const classes = element.classList;
@@ -18,29 +23,52 @@ document.addEventListener("DOMContentLoaded", function() {
         element.classList.add(`ex-animate__${afterUnderscore}`)
 
 
-var styleSheets = document.styleSheets;
-
-for (var i = 0; i < styleSheets.length; i++) {
-    var styleSheet = styleSheets[i];
-    if (styleSheet.href && styleSheet.href.includes('animated.css')) {
-        for (var j = 0; j < styleSheet.cssRules.length; j++) {
-            var rule = styleSheet.cssRules[j];
-            if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === `${afterUnderscore}`) {
-                for (var k = 0; k < rule.cssRules.length; k++) {
-                    var keyframe = rule.cssRules[k];
-                    if (keyframe.type === CSSRule.KEYFRAME_RULE && keyframe.keyText === '0%') {
-                        var keyframeCSS = keyframe.cssText;
-                        const startT = keyframeCSS.substr(4);
-                        const endT = startT.substr(0, startT.length - 1)
-                        element.style.cssText = `${endT}`;
-                        break; 
+    var styleSheets = document.styleSheets;
+        if(afterUnderscore.includes("In")){
+        for (var i = 0; i < styleSheets.length; i++) {
+            var styleSheet = styleSheets[i];
+            if (styleSheet.href && styleSheet.href.includes('animated.css')) {
+                for (var j = 0; j < styleSheet.cssRules.length; j++) {
+                    var rule = styleSheet.cssRules[j];
+                    if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === `${afterUnderscore}`) {
+                        for (var k = 0; k < rule.cssRules.length; k++) {
+                            var keyframe = rule.cssRules[k];
+                            if (keyframe.type === CSSRule.KEYFRAME_RULE && keyframe.keyText === '0%') {
+                                var keyframeCSS = keyframe.cssText;
+                                const startT = keyframeCSS.substr(4);
+                                const endT = startT.substr(0, startT.length - 1)
+                                element.style.cssText = `${endT}`;
+                                break; 
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-    }
-}
-})
+        else if(afterUnderscore.includes("Out")){
+        for (var i = 0; i < styleSheets.length; i++) {
+            var styleSheet = styleSheets[i];
+            if (styleSheet.href && styleSheet.href.includes('animated.css')) {
+                for (var j = 0; j < styleSheet.cssRules.length; j++) {
+                    var rule = styleSheet.cssRules[j];
+                    if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === `${afterUnderscore}`) {
+                        for (var k = 0; k < rule.cssRules.length; k++) {
+                            var keyframe = rule.cssRules[k];
+                            if (keyframe.type === CSSRule.KEYFRAME_RULE && keyframe.keyText === '100%') {
+                                var keyframeCSS = keyframe.cssText;
+                                const startT = keyframeCSS.substr(4);
+                                const endT = startT.substr(0, startT.length - 1)
+                                element.style.cssText = `${endT}`;
+                                break; 
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
     
     function handleScrollAnimation() {
         elements.forEach((element)=>{
@@ -49,9 +77,21 @@ for (var i = 0; i < styleSheets.length; i++) {
                 const filteredClasses = Array.from(classes).filter(className => className !== 'animate__animated');
                 const extractedWords = filteredClasses.join(' ');
                 var afterUnderscore = extractedWords.split("__")[1];
+                if(afterUnderscore.includes("In")){
                 element.classList.add(`animate__${afterUnderscore}`)
                 element.classList.remove(`ex-animate__${afterUnderscore}`)
+                }
             } 
+            if (isScrolledOutView(element)) {
+                const classes = element.classList;
+                const filteredClasses = Array.from(classes).filter(className => className !== 'animate__animated');
+                const extractedWords = filteredClasses.join(' ');
+                var afterUnderscore = extractedWords.split("__")[1];
+                if(afterUnderscore.includes("Out")){
+                element.classList.add(`animate__${afterUnderscore}`)
+                element.classList.remove(`ex-animate__${afterUnderscore}`)
+                }
+            }
         })
     }
 
