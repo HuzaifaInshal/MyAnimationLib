10030a4445ef3c315268d81212fc3b3decec50e1
HuzaifaInshal <huzaifainshal@gmail.com>
2023-12-24 19:42:00 +0500

    improved functionality

--- animated.js
+++ animated.js
@@ -1,102 +1,115 @@
-
 document.addEventListener("DOMContentLoaded", function() {
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const topThreshold = windowHeight * 0.1; // 10% from the top
+        const bottomThreshold = windowHeight * 0.1; // 10% from the bottom
+    
+        return (
+          rect.top >= topThreshold &&
+          rect.bottom <= (windowHeight - bottomThreshold)
+        );
+      }
     const elements = document.querySelectorAll('.animate__animated');
 
-        function isScrolledIntoView(el) {
-        const rect = el.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        return rect.bottom <= windowHeight * 0.8;
-      }
-        function isScrolledOutView(el) {
-        const rect = el.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        return rect.bottom <= windowHeight * 0.2;
-      }
-
     elements.forEach((element)=>{
-        const classes = element.classList;
-        const filteredClasses = Array.from(classes).filter(className => className !== 'animate__animated');
-        const extractedWords = filteredClasses.join(' ');
-        var afterUnderscore = extractedWords.split("__")[1];
-   
-        element.classList.remove(`animate__${afterUnderscore}`)
-        element.classList.add(`ex-animate__${afterUnderscore}`)
-
-
-    var styleSheets = document.styleSheets;
-        if(afterUnderscore.includes("In")){
-        for (var i = 0; i < styleSheets.length; i++) {
-            var styleSheet = styleSheets[i];
-            if (styleSheet.href && styleSheet.href.includes('animated.css')) {
-                for (var j = 0; j < styleSheet.cssRules.length; j++) {
-                    var rule = styleSheet.cssRules[j];
-                    if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === `${afterUnderscore}`) {
-                        for (var k = 0; k < rule.cssRules.length; k++) {
-                            var keyframe = rule.cssRules[k];
-                            if (keyframe.type === CSSRule.KEYFRAME_RULE && keyframe.keyText === '0%') {
-                                var keyframeCSS = keyframe.cssText;
-                                const startT = keyframeCSS.substr(4);
-                                const endT = startT.substr(0, startT.length - 1)
-                                element.style.cssText = `${endT}`;
-                                break; 
-                                }
-                            }
-                        }
-                    }
-                }
+        var classes = element.classList;
+        classes = Array.from(classes).filter(className => className !== 'animate__animated');
+        let inIsThere = false
+        let outIsThere = false
+        classes.forEach((classs)=>{
+            if(classs.includes('In')){
+                inIsThere = true
             }
-        }
-        else if(afterUnderscore.includes("Out")){
-        for (var i = 0; i < styleSheets.length; i++) {
-            var styleSheet = styleSheets[i];
-            if (styleSheet.href && styleSheet.href.includes('animated.css')) {
-                for (var j = 0; j < styleSheet.cssRules.length; j++) {
-                    var rule = styleSheet.cssRules[j];
-                    if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === `${afterUnderscore}`) {
-                        for (var k = 0; k < rule.cssRules.length; k++) {
-                            var keyframe = rule.cssRules[k];
-                            if (keyframe.type === CSSRule.KEYFRAME_RULE && keyframe.keyText === '100%') {
-                                var keyframeCSS = keyframe.cssText;
-                                const startT = keyframeCSS.substr(4);
-                                const endT = startT.substr(0, startT.length - 1)
-                                element.style.cssText = `${endT}`;
-                                break; 
-                                }
-                            }
-                        }
-                    }
-                }
+            if(classs.includes('Out')){
+                outIsThere = true
             }
+        });
+        if(inIsThere && !outIsThere){
+            classes.forEach((classs)=>{
+                if(classs.includes('In')){
+                    var afterUnderscore = classs.split("__")[1];
+                    let newString = afterUnderscore.replace(/In/g, "Out");
+                    element.classList.remove(`ex-animate__${afterUnderscore}`)
+                    element.classList.add(`animate__${afterUnderscore}`)   
+                    element.classList.add(`animate__${newString}`) 
+                    element.classList.add(`animate_Extra`)
+                }
+            })
         }
-    })
-    
-    function handleScrollAnimation() {
+        
+    });
+
+    function handleScrollAnimation(){
         elements.forEach((element)=>{
-            if (isScrolledIntoView(element)) {
-                const classes = element.classList;
-                const filteredClasses = Array.from(classes).filter(className => className !== 'animate__animated');
-                const extractedWords = filteredClasses.join(' ');
-                var afterUnderscore = extractedWords.split("__")[1];
-                if(afterUnderscore.includes("In")){
-                element.classList.add(`animate__${afterUnderscore}`)
-                element.classList.remove(`ex-animate__${afterUnderscore}`)
+            var classes = element.classList;
+            classes = Array.from(classes).filter(className => className !== 'animate__animated');
+            let inIsThere = false
+            let outIsThere = true
+            classes.forEach((classs)=>{
+                if(classs.includes('In')){
+                    inIsThere = true
                 }
-            } 
-            if (isScrolledOutView(element)) {
-                const classes = element.classList;
-                const filteredClasses = Array.from(classes).filter(className => className !== 'animate__animated');
-                const extractedWords = filteredClasses.join(' ');
-                var afterUnderscore = extractedWords.split("__")[1];
-                if(afterUnderscore.includes("Out")){
-                element.classList.add(`animate__${afterUnderscore}`)
-                element.classList.remove(`ex-animate__${afterUnderscore}`)
+                if(classs.includes('Extra')){
+                    outIsThere = false
+                }
+            });
+            if(inIsThere && outIsThere){
+                if(isElementInViewport(element)){
+                    var classes = element.classList;
+                    classes = Array.from(classes).filter(className => className !== 'animate__animated');
+                    classes.forEach((classs)=>{
+                        if(classs.includes('In')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`ex-animate__${afterUnderscore}`)
+                            element.classList.add(`animate__${afterUnderscore}`)
+                        }
+                        if(classs.includes('Out')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`animate__${afterUnderscore}`)
+                            element.classList.add(`ex-animate__${afterUnderscore}`)
+                        }
+                    })               
+                }
+                else{
+                    var classes = element.classList;
+                    classes = Array.from(classes).filter(className => className !== 'animate__animated');
+                    classes.forEach((classs)=>{
+                        if(classs.includes('Out')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`ex-animate__${afterUnderscore}`)
+                            element.classList.add(`animate__${afterUnderscore}`)
+                        }
+                        if(classs.includes('In')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`animate__${afterUnderscore}`)
+                            element.classList.add(`ex-animate__${afterUnderscore}`)
+                        }
+                    })
+                }
+            }
+            else if(inIsThere && !outIsThere){
+                if(isElementInViewport(element)){
+                    var classes = element.classList;
+                    classes = Array.from(classes).filter(className => className !== 'animate__animated');
+                    classes.forEach((classs)=>{
+                        if(classs.includes('In')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`ex-animate__${afterUnderscore}`)
+                            element.classList.add(`animate__${afterUnderscore}`)
+                        }
+                        if(classs.includes('Out')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`animate__${afterUnderscore}`)
+                            element.classList.remove(`animate__Extra`)
+                        }
+                    })               
                 }
             }
         })
     }
 
-
-
 window.addEventListener("scroll", handleScrollAnimation);
 handleScrollAnimation();
-  });
+});
