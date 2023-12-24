a9315e9bf594b8801d3ab2a172fd4f8ade8ed71e
HuzaifaInshal <huzaifainshal@gmail.com>
2023-12-24 20:01:44 +0500

    added-non-in-out-functionality

--- animated.js
+++ animated.js
@@ -19,21 +19,31 @@ document.addEventListener("DOMContentLoaded", function() {
         let inIsThere = false
         let outIsThere = false
         classes.forEach((classs)=>{
-            if(classs.includes('In')){
+            if(classs.includes('In') && classs.includes('animate')){
                 inIsThere = true
             }
-            if(classs.includes('Out')){
+            if(classs.includes('Out')  && classs.includes('animate')){
                 outIsThere = true
             }
         });
         if(inIsThere && !outIsThere){
             classes.forEach((classs)=>{
-                if(classs.includes('In')){
+                if(classs.includes('In')  && classs.includes('animate')){
                     var afterUnderscore = classs.split("__")[1];
                     let newString = afterUnderscore.replace(/In/g, "Out");
                     element.classList.remove(`ex-animate__${afterUnderscore}`)
                     element.classList.add(`animate__${afterUnderscore}`)   
                     element.classList.add(`animate__${newString}`) 
+                    element.classList.add(`animate_Extra`)
+                }
+            })
+        }
+        if(!inIsThere && !outIsThere){
+            classes.forEach((classs)=>{
+                if(classs.includes('animate')){
+                    var afterUnderscore = classs.split("__")[1];
+                    element.classList.remove(`animate__${afterUnderscore}`)
+                    element.classList.add(`ex-animate__${afterUnderscore}`)
                     element.classList.add(`animate_Extra`)
                 }
             })
@@ -105,6 +115,20 @@ document.addEventListener("DOMContentLoaded", function() {
                             element.classList.remove(`animate__Extra`)
                         }
                     })               
+                }
+            }
+            else if(!inIsThere && !outIsThere){
+                if(isElementInViewport(element)){
+                    var classes = element.classList;
+                    classes = Array.from(classes).filter(className => className !== 'animate__animated');
+                    classes.forEach((classs)=>{
+                        if(classs.includes('animate')){
+                            var afterUnderscore = classs.split("__")[1];
+                            element.classList.remove(`ex-animate__${afterUnderscore}`)
+                            element.classList.add(`animate__${afterUnderscore}`)
+                            element.classList.remove(`animate__Extra`)
+                        }
+                    });
                 }
             }
         })
